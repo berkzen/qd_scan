c3d55d4688f6ef7bf7f865666817295762920a23
Berk Zengin <berkzngn@gmail.com>
2025-06-30 15:59:43 +0200

    got rid off the redundant print for config file load cases

--- config.py
+++ config.py
@@ -9,12 +9,11 @@ def load_config(path: str = "config.yaml") -> dict:
             config = yaml.safe_load(f)
             logging.info("Loaded config from {}".format(path))
             if config is None:
-                print("Config file is empty")
+                logging.error("Config file is empty")
                 raise ValueError("Config file is empty.")
             return config
         
     except FileNotFoundError:
-        print("Config file not found")
         logging.error("Configuration file not found.")
         raise
     
